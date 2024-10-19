3d23a8fad849f3b3a66ab7ea9a4666e332503288
jotaac10 <joao.cristovao.oficial@gmail.com>
2024-10-19 18:37:09 +0100

    changed main.jsx to solve syntax problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import App from './App.jsx'
+import './index.css'
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   console.error("No root element found. Please check your HTML.");
 } else {
   createRoot(rootElement).render(
-    <StrictMode>
+    <React.StrictMode>
       <App />
-    </StrictMode>
+      </React.StrictMode>
   );
 }
