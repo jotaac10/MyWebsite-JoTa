e4ee7c0f9968e98d71ca1b48c398c375a0d7af65
jotaac10 <joao.cristovao.oficial@gmail.com>
2024-10-19 18:32:04 +0100

    changed edit 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx'; // Ensure this file is named correctly
+
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
